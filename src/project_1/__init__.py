2399b949a93351146df594f843c7ad99d6032b92
Muhammad Mazher Khan <161349044+muhammadmazher@users.noreply.github.com>
2025-05-27 14:21:06 +0500

    Delete src/project_1 directory

--- src/project_1/__init__.py
+++
@@ -1,2 +0,0 @@
-def main() -> None :
-    print("Hello from UV project-1!, its first project of AGENTIC AI")
--- src/project_1/hello.py
+++
@@ -1,6 +0,0 @@
-def main()
-    print("Hello form project-1")
-
-
-    if __name__ == "__main__":
-        main()
