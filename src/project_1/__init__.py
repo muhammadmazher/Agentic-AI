c2f19881c151e83ccd0c43a1cade15d2d61eed4e
Muhammad Mazher Khan <muhammadmazher@gmail.com>
2025-05-27 14:16:46 +0500

    first commit

---
+++ src/project_1/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None :
+    print("Hello from UV project-1!, its first project of AGENTIC AI")
---
+++ src/project_1/hello.py
@@ -0,0 +1,6 @@
+def main()
+    print("Hello form project-1")
+
+
+    if __name__ == "__main__":
+        main()
